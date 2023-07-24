afe207cd2e8f6f29d1573dfadd474661e5e7801e
Akash-Guptag <gupta.akash258258@gmail.com>
2023-07-24 22:24:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Hero from './component/Hero';
+import Demo from './component/Demo';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      <div className='main'>
+        <div className='gradient' />
+      </div>
+
+      <div className='app'>
+      <Hero />
+      <Demo />
+      </div>
+    </main>
+  )
+}
+
+export default App
